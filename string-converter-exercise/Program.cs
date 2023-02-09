89a9798be6870db628139885fe77606b68272f80
Adam Lee <adamlee@Adams-MacBook-Air.local>
2023-02-09 10:33:47 -0700

    added string converter exercise

---
+++ string-converter-exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using SCE.Services;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("string-converter-exercise");
+        var stringConverter = new strConverter();
+        var string1 = "The answer is 42. What's the question?";
+        var resultOne = strConverter.ReplacePunctuationWithVowel(string1);
+
+        Console.WriteLine(string1);
+        Console.WriteLine(resultOne); 
+    }
+}
---
+++ string-converter-exercise/Services/strConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SCE.Services
+{
+	public class strConverter
+	{
+		public strConverter()
+		{
+		}
+
+		public static string ReplacePunctuationWithVowel(string s)
+		{
+			var result = s
+					.Replace(" ", "a")
+					.Replace(".", "e")
+					.Replace("!", "i")
+					.Replace("\'", "o")
+					.Replace("?", "u");
+
+			return result; 
+		}
+
+		public static string ReplaceVowelWithNumber(string s)
+		{
+			int vowelCounter = 0;
+			string result = "";
+			foreach(char c in s)
+			{
+				if(c.Equals('a') && vowelCounter > 9)
+				{
+					vowelCounter++;
+					result.Replace(c, '1');
+				}
+			}
+
+			return result;
+		}
+	}
+}
+
