fcfce886356be9e5a4e889e88ca80e0cb291b8bb
Adam Lee <adamlee@Adams-MacBook-Air.local>
2023-02-10 00:29:42 -0700

    Added draft unit test case

--- string-converter-exercise-tests/UnitTest1.cs
+++ string-converter-exercise-tests/UnitTest1.cs
@@ -1,10 +1,25 @@
-﻿namespace string_converter_exercise_tests;
+﻿
+
+using System; 
+using SCE;
+using SCE.Services;
+
+namespace string_converter_exercise_tests;
 
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void ConvertFirstEightVowels_Success()
     {
+        // Arrange
+        var strConverter = new StrConverter(); 
+        var testStringOne = "Hola Amigo";
+
+        // Act
+        var sut = strConverter.ReplaceVowelWithNumber(testStringOne);
+
+        // Assert
+        Assert.Equal("H4l1 1m3g4", sut); 
 
     }
 }
